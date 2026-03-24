0ec18127c00dbd2879e7bf38c9d88b26bba5bdc9
Manikanta-SRE <manigcpcloud@gmail.com>
2026-03-24 10:56:49 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,59 @@
+resource "google_iam_workload_identity_pool" "github_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "GitHub Actions Pool"
+}
 
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.owner"      = "assertion.repository_owner"
+  }
+  
+  # IMPORTANT: Update with your exact GitHub Username/RepoName if you haven't already!
+  attribute_condition = "assertion.repository == 'Manikanta-SRE/devops-applicaton'"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account" "github_actions_sa" {
+  account_id   = "github-actions-deployer"
+  display_name = "Service Account for GitHub Actions"
+}
+
+resource "google_service_account_iam_member" "wif_impersonation" {
+  service_account_id = google_service_account.github_actions_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/Manikanta-SRE/devops-applicaton"
+}
+
+resource "google_project_iam_member" "run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
+
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
+
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
+
+output "workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github_provider.name
+}
+
+output "service_account_email" {
+  value = google_service_account.github_actions_sa.email
+}
